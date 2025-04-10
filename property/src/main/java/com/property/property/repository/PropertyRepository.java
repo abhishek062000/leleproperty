61463602a67bc520a3296998bede05c551cf1bc6
ishan199811 <ishansharma199811@gmail.com>
2025-04-11 00:10:02 +0530

    complete update

---
+++ property/src/main/java/com/property/property/repository/PropertyRepository.java
@@ -0,0 +1,18 @@
+package com.property.property.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.property.property.model.dao.Property;
+
+
+
+
+
+public interface PropertyRepository extends JpaRepository<Property , Long>{
+	
+	
+	
+	
+	
+
+}
---
+++ property/src/main/java/com/property/property/repository/RoleRepository.java
@@ -0,0 +1,18 @@
+package com.property.property.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.property.property.model.dao.Role;
+
+
+
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+	Role findByName(String string);
+  
+}
