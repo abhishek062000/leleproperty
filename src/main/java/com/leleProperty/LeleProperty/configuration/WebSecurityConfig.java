475797f94d55f868b6e408550bd164e6d59a37a8
ishan199811 <ishansharma199811@gmail.com>
2025-04-10 16:38:29 +0530

    config update

--- src/main/java/com/leleProperty/LeleProperty/configuration/WebSecurityConfig.java
+++ src/main/java/com/leleProperty/LeleProperty/configuration/WebSecurityConfig.java
@@ -54,8 +54,10 @@ public class WebSecurityConfig {
                         "/api/register",
                         "/property","/api/getAllProperty","/productlist","/service","/contact","/**").permitAll() // Allow Swagger UI
                 
-               
-               
+                .requestMatchers("/us").hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
+                .requestMatchers("/admin/**").hasAnyAuthority("ADMIN", "CREATOR")
+                .requestMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
+                .requestMatchers("/delete/**").hasAuthority("ADMIN")
                 
                 .anyRequest().authenticated()
         )
